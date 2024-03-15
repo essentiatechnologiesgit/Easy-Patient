6f6ddb249ad07efe66715871d0484532afb946d4
Misbah Ali <136062372+MisbahAli1111@users.noreply.github.com>
2024-03-15 10:41:29 +0500

    Login Screen integration completed

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -6,6 +6,9 @@ import AlertIcon from '../components/AlertIcon';
 import ValidationError from '../components/ValidationError';
 import Snackbar from '../components/Snackbar';
 import ModalLoader from '../components/ModalLoader';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import qs from 'qs';
 const LoginScreen = () => {
   const navigation = useNavigation();
   const [username, setUsername] = useState('');
@@ -17,8 +20,8 @@ const LoginScreen = () => {
   const [invalidEmail, setInvalidEmail] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarKey, setSnackbarKey] = useState(0);
-  const [showLoader , setShowLoader] = useState(false);
-
+  const [showLoader, setShowLoader] = useState(false);
+  const [emailExist, setEmailExist] = useState(true);
   const handleLogin = () => {
     setUsernameError(false);
     setPasswordError(false);
@@ -33,37 +36,75 @@ const LoginScreen = () => {
       setInvalidEmail(true);
       setErrorMessage("Invalid email");
     }
-    else if (checkEmailExist(username)) {
-      handleShowSnackbar("Incorrect Username/E-mail")
-    }
-    else if (!showPasswordInput) {
-      setShowPasswordInput(true);
+    else if (emailExist) {
+      checkEmailExist();
     }
     else if (!password) {
       setPasswordError(true);
       setErrorMessage("provide password");
     }
     else {
-      console.warn("Done");
+      Login();
     }
   };
+
+  const Login = async () => {
+    let data = qs.stringify({
+      'grant_type': 'password',
+      'username': username,
+      'password': password
+    });
+  
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api-patient-dev.easy-health.app/o/token/',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ZWZmZWN0aXZlc2FsZXNfd2ViX2NsaWVudDo4dz9keF5wVUVxYiZtSnk/IWpBZiNDJWtOOSFSMkJaVQ=='
+      },
+      data: data
+    };
+  
+    try {
+      const response = await axios.request(config);
+      console.log(JSON.stringify(response.data));
+      await AsyncStorage.setItem('loginResponse', JSON.stringify(response.data));
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
 
   const handleShowSnackbar = (message) => {
     setSnackbarMessage(message);
     setSnackbarKey((prevKey) => prevKey + 1);
   };
 
-  const checkEmailExist = async () => {
-    setShowLoader(true);
-    setTimeout(() => {
-      setTimeout(() => {
-        setShowLoader(false);
-        return true;
-      }, 2000);
-    }, 2000);
-    
+  const checkEmailExist = () => {
+    checkEmail();
   };
-  
+
+  const checkEmail = async () => {
+    try {
+      setShowLoader(true);
+      const response = await axios.get(`https://api-patient-dev.easy-health.app/patient/${username}`);
+      if (response.data.registered === true) {
+        setEmailExist(false);
+        setShowPasswordInput(true);
+      } else {
+        handleShowSnackbar("Incorrect Username/E-mail");
+      }
+      setShowLoader(false);
+    } catch (error) {
+      console.error('Error:', error);
+      setShowLoader(false);
+    }
+  };
+
 
   const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;

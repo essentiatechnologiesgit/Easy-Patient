a61755bec48f0205a239df75c937c1c57295dcc4
essentiatechnologiesgit <fernando@essentia.tech>
2024-08-13 16:01:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ export default {
     buttonText:'#FFFFFF',
     headerColor:'#FFFFFF',
     fontStyle:'OpenSans-Regular',
-    identifier:'com.easypatientdynamic'
+    identifier:'com.easypatientdynamic',
+    name:'NewApp'
   };
 

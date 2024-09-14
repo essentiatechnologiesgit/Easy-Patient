1ea959044a8e5124d3ef81f05b5c73d161381254
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-14 17:13:11 +0500

    Update config.js

---
+++ src/config.js
@@ -0,0 +1,16 @@
+{
+  "primaryColor": "#795252",
+  "secondaryColor": "#473535",
+  "tertiaryColor": "#6d2d2d",
+  "buttonColor": "#6d5858",
+  "modalColor": "#745252",
+  "buttonTextColor": "#490101",
+  "headerColor": "#361010",
+  "appName": "sjdh",
+  "shortDescription": "kj",
+  "fullDescription": "j",
+  "appCategory": "j",
+  "contentRating": "j",
+  "releaseNotes": "j",
+  "tags": "j"
+}

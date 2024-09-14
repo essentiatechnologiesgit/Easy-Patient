b033a2c4e3040dcaab0db908b8d501beadc58892
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-14 22:06:53 +0500

    Upload config.js

---
+++ src/assets/config.js
@@ -0,0 +1,30 @@
+
+    const config = {
+      splashScreen: require('src/assets/screenshot_dashboard.png'),
+      primaryColor: '#795252',
+      secondaryColor: '#473535',
+      tertiaryColor: '#6d2d2d',
+      logo: require('src/assets/screenshot_dashboard.png'),
+      backgroundImage: require('src/assets/04.png'),
+      backgroundColorImage: '',
+      backgroundImageType: 'png',
+      subLogo: require('src/assets/screenshot_dashboard.png'),
+      backgroundColor: '',
+      modalColor: '#745252',
+      buttonText: '#490101',
+      headerColor: '#865a5a',
+      fontStyle: 'OpenSans-Regular',
+      BUNDLE_ID: 'com.org.J',
+      Name: 'j',
+      appIcon: 'src/assets/04.png',
+      shortDescription: 'j',
+      fullDescription: 'j',
+      appCategory: 'j',
+      contentRating: 'j',
+      releaseNotes: 'j',
+      tags: 'j',
+      buttonTextColor: '#490101'
+    };
+    
+    module.exports = config;
+    

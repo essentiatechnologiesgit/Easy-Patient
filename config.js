c2c07655ee51b393badd831dcd90ca073548d349
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-18 11:41:23 +0500

    Update config.js and upload new files

--- config.js
+++ config.js
@@ -1,30 +1,31 @@
 
     const config = {
-    splashScreen: require('./src/assets/newImage.png'),
-      primaryColor: '#795252',
-      secondaryColor: '#473535',
-      tertiaryColor: '#422424',
+      splashScreen: require('./src/assets/newImage.png'),
+      primaryColor: '#895555',
+      textColorHeadings: '#2A2A31',
+      secondaryColor: '#ffffff',
+      tertiaryColor: '#6d2d2d',
       logo: require('./src/assets/newImage.png'),
       backgroundImage: require('./src/assets/newImage.png'),
       backgroundColorImage: '',
       backgroundImageType: 'png',
-      subLogo: require('./src/assets/newImage.png'),
-      backgroundColor: '',
-      modalColor: '#745252',
+      subLogo:require('./src/assets/newImage.png'),
+      backgroundColor: '#FFFFFF',
+      modalColor: '#8a5252',
       buttonText: '#490101',
-      headerColor: '#361010',
-      fontStyle: 'OpenSans-Regular',
-      BUNDLE_ID: 'com.org.Ui',
-      Name: 'ui',
-      appIcon: require('./src/assets/newImage.png'),
-      shortDescription: 'ui',
-      fullDescription: 'b',
-      appCategory: 'h',
-      contentRating: 'b',
-      releaseNotes: 'h',
-      tags: 'b',
-      buttonTextColor: '#490101'
+      headerColor: '#865a5a',
+      BUNDLE_ID: 'com.org.easypatient',
+      Name: 'asd',
+      ISSUER_ID: "asd",
+      KEY_ID: "asd",
+      private_key: '
+        -----BEGIN PRIVATE KEY-----
+  MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgmtzyTVPL1cphRZjf
+  pzIlE/6pV5OFCS6zaUoyqBIHflygCgYIKoZIzj0DAQehRANCAASQxfZ0NyqZre50
+  6NGowYahQsvqEQew5MStUc94chxoi8QqtVNTOcsgzqflnQ/v7YeETnnUX+P+2+2E
+  +EG+nw8t
+  -----END PRIVATE KEY-----
+      ',
     };
-    
     module.exports = config;
     

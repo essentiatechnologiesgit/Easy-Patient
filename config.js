c6a0648189967930979a65db0fcca3d4714ee971
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-19 12:43:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,34 @@
 
-    const config = {
-      splashScreen: require('./src/assets/newImage.png'),
-      primaryColor: '#895555',
-      textColorHeadings: '#2A2A31',
-      secondaryColor: '#ffffff',
-      tertiaryColor: '#6d2d2d',
-      logo: require('./src/assets/newImage.png'),
-      backgroundImage: require('./src/assets/newImage.png'),
-      backgroundColorImage: '',
-      backgroundImageType: 'png',
-      subLogo:require('./src/assets/newImage.png'),
-      backgroundColor: '#FFFFFF',
-      modalColor: '#8a5252',
-      buttonText: '#490101',
-      headerColor: '#865a5a',
-      BUNDLE_ID: 'com.org.easypatient',
-      Name: 'asd',
-      ISSUER_ID: "ad39d400-3314-48f8-977c-29a272eebad0",
-      KEY_ID: "74H4QHFPVP",
-      private_key: '
-        -----BEGIN PRIVATE KEY-----
+const config = {
+  splashScreen: require('./src/assets/splash.jpg'),
+  primaryColor: '#82848D',
+  textColorHeadings: '#2A2A31',
+  secondaryColor: '#C1AB60',
+  tertiaryColor: '#fff',
+  logo: require('./src/assets/logo.png')  ,
+  backgroundImage: require('./src/assets/background-image.png'),
+  backgroundColorImage: '#EEF6F8',
+  backgroundImageType: 'png',
+  subLogo: require('./src/assets/sub-logo.png'),
+  backgroundColor: '#FFFFFF',
+  modalColor: '#FFFFFF',
+  buttonText: '#FFFFFF',
+  headerColor: '#FFFFFF',
+  fontStyle: 'OpenSans-Regular',
+  BUNDLE_ID: 'com.org.easyPatientTesting6',
+  NAME: 'Easy Patient Testing 6',
+  ISSUER_ID:"ad39d400-3314-48f8-977c-29a272eebad0",
+  KEY_ID:"74H4QHFPVP",
+  private_key:`
+  -----BEGIN PRIVATE KEY-----
   MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgmtzyTVPL1cphRZjf
   pzIlE/6pV5OFCS6zaUoyqBIHflygCgYIKoZIzj0DAQehRANCAASQxfZ0NyqZre50
   6NGowYahQsvqEQew5MStUc94chxoi8QqtVNTOcsgzqflnQ/v7YeETnnUX+P+2+2E
   +EG+nw8t
   -----END PRIVATE KEY-----
-      ',
-    };
-    module.exports = config;
+  `,
+};
+module.exports = config;
     
 
 // const config = {

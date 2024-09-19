5b261ac5be7c507cad9062be37a3084600d9e687
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-19 11:42:23 +0500

    Update config.js and upload new files

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 
-    const config = {
-      splashScreen: require('./src/assets/newImage.png'),
-      primaryColor: '#895555',
-      textColorHeadings: '#2A2A31',
-      secondaryColor: '#ffffff',
-      tertiaryColor: '#6d2d2d',
-      logo: require('./src/assets/newImage.png'),
-      backgroundImage: require('./src/assets/newImage.png'),
-      backgroundColorImage: '',
-      backgroundImageType: 'png',
-      subLogo:require('./src/assets/newImage.png'),
-      backgroundColor: '#FFFFFF',
-      modalColor: '#8a5252',
-      buttonText: '#490101',
-      headerColor: '#865a5a',
-      BUNDLE_ID: 'com.org.easypatient',
-      Name: 'asd',
-      ISSUER_ID: "asd",
-      KEY_ID: "asd",
-      private_key: '
-        -----BEGIN PRIVATE KEY-----
+const config = {
+splashScreen: require('./src/assets/arrow.png'),
+primaryColor: '#683c3c',
+textColorHeadings: '#2A2A31',
+secondaryColor: '#473535',
+tertiaryColor: '#6d2d2d',
+logo: require('./src/assets/archiveBlack.png'),
+backgroundImage: require('./src/assets/attestations.png'),
+backgroundColorImage: '',
+backgroundImageType: 'png',
+subLogo:require('./src/assets/arrow.png'),
+backgroundColor: '#FFFFFF',
+modalColor: '#8a5252',
+buttonText: '#754949',
+headerColor: '#813636',
+BUNDLE_ID: 'com.org.easyPatient',
+Name: 'asnd',
+ISSUER_ID: "asd_dsjaa_sdmm",
+KEY_ID: "adsd",
+private_key: '
+  -----BEGIN PRIVATE KEY-----
   MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgmtzyTVPL1cphRZjf
   pzIlE/6pV5OFCS6zaUoyqBIHflygCgYIKoZIzj0DAQehRANCAASQxfZ0NyqZre50
   6NGowYahQsvqEQew5MStUc94chxoi8QqtVNTOcsgzqflnQ/v7YeETnnUX+P+2+2E
   +EG+nw8t
   -----END PRIVATE KEY-----
-      ',
-    };
-    module.exports = config;
+',
+};
+module.exports = config;
     

5ad34579f851fdafd707d2b30a11c1c04d6f09c6
essentiatechnologiesgit <fernando@essentia.tech>
2024-09-19 12:40:47 +0500

    Update config.js and upload new files

--- config.js
+++ config.js
@@ -1,21 +1,32 @@
 
-const config = {
-splashScreen: require('./src/assets/AttestationsGold.png'),
-primaryColor: '#683c3c',
-textColorHeadings: '#2A2A31',
-secondaryColor: '#473535',
-tertiaryColor: '#5c2929',
-logo: require('./src/assets/AttestationsGold.png'),
-backgroundImage: require('./src/assets/deleteWhite.png'),
-backgroundColorImage: '',
-backgroundImageType: 'png',
-subLogo:require('./src/assets/bellIcon.png'),
-backgroundColor: '#FFFFFF',
-modalColor: '#8a5252',
-buttonText: '#754949',
-headerColor: '#8a3f3f',
-BUNDLE_ID: 'sdf.org.easypatient',
-Name: 'rew',
-ISSUER_ID: "adsasda",
-KEY_ID: "adsasd",
-private_key: 
+    const config = {
+      splashScreen: require('./src/assets/AttestationsGold.png'),
+      primaryColor: '#683c3c',
+      textColorHeadings: '#2A2A31',
+      secondaryColor: '#473535',
+      tertiaryColor: '#5c2929',
+      logo: require('./src/assets/AttestationsGold.png'),
+      backgroundImage: require('./src/assets/background-image.png'),
+      backgroundColorImage: '',
+      backgroundImageType: 'png',
+      subLogo: require('./src/assets/bellIcon.png'),
+      backgroundColor: '#FFFFFF',
+      modalColor: '#8a5252',
+      buttonText: '#754949',
+      headerColor: '#8a3f3f',
+      BUNDLE_ID: 'gdg.org.easyatads',
+      Name: 'df',
+      ISSUER_ID: 'asdads',
+      KEY_ID: 'adsda',
+      private_key: `
+      -----BEGIN PRIVATE KEY-----
+  MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgmtzyTVPL1cphRZjf
+  pzIlE/6pV5OFCS6zaUoyqBIHflygCgYIKoZIzj0DAQehRANCAASQxfZ0NyqZre50
+  6NGowYahQsvqEQew5MStUc94chxoi8QqtVNTOcsgzqflnQ/v7YeETnnUX+P+2+2E
+  +EG+nw8t
+  -----END PRIVATE KEY-----
+      `,
+    };
+    
+    module.exports = config;
+    
